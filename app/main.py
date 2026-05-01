1ca2b9546d8fd25ed17a9335a7fee1767a783799
Cherry <hackermengzhi@gmail.com>
2026-05-01 23:03:46 +1200

    Init

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Query
-from app.weather import get_weather
+from app.weather import get_weather, get_forcast_weather
 
 app = FastAPI(title="Weather API", version="1.0")
 
@@ -13,4 +13,16 @@ async def weather_by_city(city: str = Query(..., description="City name, e.g. Au
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Weather service error")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+@app.get("/forecast")
+async def forecast_by_city(city: str = Query(..., description="City name, e.g. Auckland")):
+    try:
+        return await get_forcast_weather(city)
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
--- app/weather.py
+++ app/weather.py
@@ -3,13 +3,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
-
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-
-
+# API_KEY = os.getenv("OPENWEATHER_API_KEY")
+API_KEY= "75b20b4644770f5eb51c2231c3cc7dbc"
+BASE_URL = "https://api.openweathermap.org"
 async def get_coordinates(client, city):
-    response = await client.get("http://api.openweathermap.org/geo/1.0/direct", params={
+    response = await client.get(BASE_URL+ "/geo/1.0/direct", params={
         "q": city,
         "limit": 1,
         "appid": API_KEY
@@ -23,7 +21,7 @@ async def get_coordinates(client, city):
 async def get_weather(city: str) -> dict:
     async with httpx.AsyncClient() as client:
         lat, lon, resolved_name, country = await get_coordinates(client, city)
-        response = await client.get(BASE_URL, params={
+        response = await client.get(BASE_URL+"/data/2.5/weather", params={
             "lat": lat,
             "lon": lon,
             "appid": API_KEY,
@@ -38,4 +36,31 @@ async def get_weather(city: str) -> dict:
             "feels_like_c": data["main"]["feels_like"],
             "description": data["weather"][0]["description"],
             "humidity_percent": data["main"]["humidity"]
+        }
+
+async def get_forcast_weather(city) -> dict:
+    async with httpx.AsyncClient() as client:
+        lat, lon, resolved_name, country = await get_coordinates(client, city)
+        response = await client.get(BASE_URL+ "/data/2.5/forecast", params={
+            "lat": lat,
+            "lon": lon,
+            "appid": API_KEY,
+            "cnt": 5
+        })
+        response.raise_for_status()
+        data = response.json()
+        forecast = []
+        for item in data["list"]:
+            forecast.append({
+                "datetime": item["dt_txt"],
+                "temperature_c": item["main"]["temp"],
+                "feels_like_c": item["main"]["feels_like"],
+                "description": item["weather"][0]["description"],
+                "humidity_percent": item["main"]["humidity"],
+                "rain_probability": item.get("pop", 0)  # probability of precipitation
+            })
+        return {
+            "city": resolved_name,
+            "country": country,
+            "forecast": forecast
         }
